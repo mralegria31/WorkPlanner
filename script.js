5dc8ffe18c4ef13505341cbfa516cbb913f29af3
Francisco Alegria <44007763+mralegria31@users.noreply.github.com>
2022-01-23 21:09:16 -0500

    alt js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-Var date= moment().format("MMM Do YY");
+
+
+
+
+
+
+
+
+var saveButton = document.querySelector(".saveBtn");
+today.textContent = moment().format('MMMM Do YYYY, h:mm:ss a');
+saveButton.addEventListener("click", saveTask);
+
+textArea.value = localStorage.getItem("Things to do");
+function saveTask() {
+    localStorage.setItem("Things to do", textArea.value);
+};
